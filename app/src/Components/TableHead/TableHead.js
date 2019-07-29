fb09a73e7c4e9f59fd7fd16c0b7c7a9056d6f223
Konstantin <kostilios@yandex.ru>
2019-07-29 13:35:06 +0300

    table fix

--- app/src/Components/TableHead/TableHead.js
+++ app/src/Components/TableHead/TableHead.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './TableHead.css'
 
 const TableHead = ( {groupBy} ) => {
+  let groupByName=groupBy.split(/(?=[A-Z])/);
+  if (groupByName.length < 2) {
+    groupByName = groupBy
+  } else {
+    groupByName = groupByName.join(' ')
+  }
+
     return (
       <thead>
         <tr>
-          <th scope="col">{groupBy.toUpperCase()}</th>
+          <th scope="col">{groupByName.toUpperCase()}</th>
           <th scope="col">IMPRESSIONS</th>
           <th scope="col">CLICKS</th>
           <th scope="col">MONEY</th>
