92826dd08ed5c1b0523bec8f707f3bbafe2c2402
Emil Muradov <emuradov@wowworks.ru>
2020-05-27 15:00:13 +0400

    add draft folder structure for state store, add redux ecosystem, add basic types

---
+++ app/src/store/types.ts
@@ -0,0 +1,35 @@
+export interface DataRow {
+  day?: string
+  platform?: string
+  impressions: number
+  clicks: number
+  money: number
+}
+
+export interface Query {
+  groupBy: string
+  from: string
+  to: string
+  limit: number
+  offset: number
+  platform: string
+  browsers: string | string[]
+  operatingSystems: string | string[]
+}
+
+export interface SelectListItem {
+  label: string
+  value: number | string
+  platform?: number
+}
+
+export interface Statistics {
+  count: number
+  rows: DataRow[]
+  total: number
+}
+
+export interface Store {
+  data: DataRow[]
+  query: Query
+}
