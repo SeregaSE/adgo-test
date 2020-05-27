92cae057d12df4d725bb0d55a1f920a3e0acc30d
Emil Muradov <emuradov@wowworks.ru>
2020-05-27 13:12:52 +0400

    layout in progress

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,48 @@
 import React, { Component } from 'react'
 
-import { DatePicker, Select } from 'antd'
+import { DatePicker, Select, Table } from 'antd'
 import 'antd/dist/antd.css'
 
 import styles from './App.css'
 
+const { Option } = Select
+
+const columns = [
+  {
+    title: 'Day',
+    key: 'day'
+  },
+  {
+    title: 'Impressions',
+    key: 'impressions'
+  },
+  {
+    title: 'Conversions',
+    key: 'conversions'
+  },
+  {
+    title: 'Money',
+    key: 'money'
+  }
+]
 
 export class App extends Component<any, any> {
     render(): React.ReactNode {
         return (
             <>
-                <div className={styles.row}>
+                <div className={styles.filtersRow}>
                     <DatePicker />
                     <DatePicker />
                     <Select></Select>
                 </div>
-                <div className={styles.row}>
-                    <Select></Select>
-                    <Select></Select>
-                    <Select></Select>
+                <div className={styles.filtersRow}>
+                    <Select defaultValue={'test'} className={styles.wideSelect}>
+                      <Option value={'test'}>Test</Option>
+                    </Select>
+                    <Select className={styles.wideSelect}></Select>
+                    <Select className={styles.wideSelect}></Select>
                 </div>
+                <Table columns={columns} />
             </>
         )
     }
