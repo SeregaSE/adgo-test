2d0091be921bdee9ba767ae3ba5ff0bca1334176
kandiana <dianka95@gmail.com>
2021-11-17 21:11:01 +0300

    removed unnecessary lines from store

--- app/src/store/store.ts
+++ app/src/store/store.ts
@@ -3,8 +3,5 @@ import thunk from 'redux-thunk';
 import { reducer, INITIAL_STATE } from './reducer';
 
 export const store = createStore(reducer, INITIAL_STATE, applyMiddleware(thunk));
-store.dispatch({ type: '' });
-
-store.subscribe(() => console.log(store.getState()));
 
 export type RootState = ReturnType<typeof reducer>;
